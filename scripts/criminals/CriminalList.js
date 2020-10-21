d3679a2de3d37c7941aa6941ec4c88cf0a56fad9
Travis Milner <travismilner1@gmail.com>
2020-10-21 09:01:20 -0500

    added functions

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,6 +1,8 @@
 import { getCriminals, useCriminals } from "./CriminalProvider.js"
 import { criminalHTML } from "./Criminal.js"
+import { useConvictions } from "../convictions/ConvictionProvider.js"
 
+const eventHub = document.querySelector(".container")
 export const CriminalList = () => {
 
     getCriminals().then(() => {
@@ -16,3 +18,38 @@ export const CriminalList = () => {
         crimContainer.innerHTML += buildCriminalHtml
     })
 }
+
+eventHub.addEventListener("crimeSelected", event => {
+    console.log("crime selected event happened")
+
+    const criminalsArray = useCriminals()
+    // console.log(criminalsArray)
+
+    const convictionsArray = useConvictions();
+
+   const convictionThatWasChosen = convictionsArray.find(convictionObj => {
+        return convictionObj.id === parseInt(event.detail.crimeThatWasChosen)
+    })
+
+    console.log(convictionThatWasChosen)
+    const filteredCriminalsArray = criminalsArray.filter(criminalObj => {
+    return criminalObj.conviction === convictionThatWasChosen.name
+        
+    })
+     const CriminalList = () => {
+
+        getCriminals().then(() => {
+            const crimArray = useCriminals()
+    
+            const crimContainer = document.querySelector(".criminalsContainer")
+    
+            let buildCriminalHtml = ""
+    
+            for (const criminal of crimArray) {
+                buildCriminalHtml += criminalHTML(criminal)
+            }
+            crimContainer.innerHTML += buildCriminalHtml
+        })
+    }
+
+})
